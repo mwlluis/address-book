b0187db4f0e8b49aea71beaa99c048e63dc120ce
Matthew Lluis <mwlluis@hotmail.com>
2020-06-04 08:47:52 -0400

    added business logic to assign ids to new contacts in address book

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
+// Business Logic for AddressBook
+
+function AddressBook() {
+  this.contacts = [];
+  this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function() {
+  this.currentId +=1;
+  return this.currentId;
+}
+
+// Business Logic for Contacts
+
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
